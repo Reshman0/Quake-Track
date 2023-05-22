5e185a8e5951ac0681ee7fd50ec1b3e413edc2c6
melike2367 <116796323+melike2367@users.noreply.github.com>
2023-05-22 19:57:24 +0300

    Add files via upload

---
+++ MAIN_PROJECT.py
@@ -0,0 +1,493 @@
+import pandas as pd
+import csv
+import random
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from pip._vendor import requests
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime
+import time
+
+def main_menu():
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("EARTHQUAKE REPORT SYSTEM")  
+
+    title_label = tk.Label(window, text="EARTHQUAKE REPORT SYSTEM", font=("Arial", 16, "bold"))
+    title_label.pack(pady=10)
+
+    options_frame = tk.Frame(window)
+    options_frame.pack(side=tk.TOP, anchor=tk.CENTER)
+
+    admin_btn = tk.Button(options_frame, text="Admin Login", command=admin_menu, width=20, height=2)
+    admin_btn.pack(pady=5)
+
+    report_btn = tk.Button(options_frame, text="Make a Report", command=user_menu, width=20, height=2)
+    report_btn.pack(pady=5)
+
+    view_btn = tk.Button(options_frame, text="View Recent Earthquakes", command=recent_earthquakes, width=20, height=2)
+    view_btn.pack(pady=5)
+
+    button = tk.Button(options_frame, text="Exit", command=window.quit, width=10, height=2)
+    button.pack(pady=5)
+
+    window.mainloop()
+
+       
+def user_menu():
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("User Menu")
+
+    title_label = tk.Label(window, text="Select an option:", font=("Arial", 16, "bold"))
+    title_label.pack(pady=10)
+
+    options_frame = tk.Frame(window)
+    options_frame.pack()
+
+    button1 = tk.Button(options_frame, text="I am trapped under rubble", command=under_rubble, width=30, height=2)
+    button1.pack(pady=5)
+
+    button2 = tk.Button(options_frame, text="I will report a debris", command=reporting_debris, width=30, height=2)
+    button2.pack(pady=5)
+
+    button3 = tk.Button(options_frame, text="I need supplies", command=supplies, width=30, height=2)
+    button3.pack(pady=5)
+    
+    button4 = tk.Button(options_frame, text="Return Main Menu", command=main_menu, width=30, height=2)
+    button4.pack(pady=5)
+
+    window.mainloop()
+
+def under_rubble():
+    def submit_report():
+        global health_condition
+
+        province = province_entry.get()
+        district = district_entry.get()
+        street_name = street_entry.get()
+        apartment_name = apartment_entry.get()
+        health_condition = health_condition_combo.get()
+
+        if apartment_name in pd.read_csv("DebrisReport.csv")["APARTMENT NAME"].tolist():
+            result_label.config(text="A report has already been made for this apartment.")
+        else:
+            report = [province, district, street_name, apartment_name, health_condition, "No help yet"]
+            with open("DebrisReport.csv", "a", newline='', encoding='utf-8') as ea:
+                writer = csv.writer(ea)
+                writer.writerow(report)
+            result_label.config(text="REPORT SUBMITTED")
+
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("I am trapped under rubble")
+
+    province_label = tk.Label(window, text="Province:", font=("Arial", 12))
+    province_label.pack(pady=10)
+
+    province_entry = tk.Entry(window, width=30)
+    province_entry.pack()
+
+    district_label = tk.Label(window, text="District:", font=("Arial", 12))
+    district_label.pack(pady=10)
+
+    district_entry = tk.Entry(window, width=30)
+    district_entry.pack()
+
+    street_label = tk.Label(window, text="Street Name:", font=("Arial", 12))
+    street_label.pack(pady=10)
+
+    street_entry = tk.Entry(window, width=30)
+    street_entry.pack()
+
+    apartment_label = tk.Label(window, text="Apartment Name:", font=("Arial", 12))
+    apartment_label.pack(pady=10)
+
+    apartment_entry = tk.Entry(window, width=30)
+    apartment_entry.pack()
+
+    health_condition_label = tk.Label(window, text="Health Condition:", font=("Arial", 12))
+    health_condition_label.pack(pady=10)
+
+    health_condition_combo = ttk.Combobox(window, values=["Heavily Injured", "Injured", "No Injuries"], width=30)
+    health_condition_combo.pack()
+
+    submit_button = tk.Button(window, text="Submit", command=submit_report, width=15)
+    submit_button.pack(pady=20)
+
+    button1 = tk.Button(window, text="Return Upper Menu", command=user_menu, width=30, height=2)
+    button1.pack(pady=5)
+
+    result_label = tk.Label(window, text="", font=("Arial", 12))
+    result_label.pack()
+
+    window.mainloop()
+
+def reporting_debris():
+    def submit_rprt():
+
+        province = province_entry.get()
+        district = district_entry.get()
+        street_name = street_entry.get()
+        apartment_name = apartment_entry.get()
+
+        
+
+        if apartment_name in (pd.read_csv("DebrisReport.csv").to_dict("list"))["APARTMENT NAME"]:
+            result_label.config(text="A report has already been made for this apartment.")
+        else:
+            report = [province, district, street_name, apartment_name] + ["not under rubble"] + ["no help yet"]
+
+            with open("DebrisReport.csv", "a", newline='', encoding='utf-8') as eb:
+                writer = csv.writer(eb)
+                writer.writerow(report)
+
+            result_label.config(text="REPORT SUBMITTED")
+
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Report A Debris")
+
+    province_label = tk.Label(window, text="Province:", font=("Arial", 12))
+    province_label.pack(pady=10)
+
+    province_entry = tk.Entry(window, width=30)
+    province_entry.pack()
+
+    district_label = tk.Label(window, text="District:", font=("Arial", 12))
+    district_label.pack(pady=10)
+
+    district_entry = tk.Entry(window, width=30)
+    district_entry.pack()
+
+    street_label = tk.Label(window, text="Street Name:", font=("Arial", 12))
+    street_label.pack(pady=10)
+
+    street_entry = tk.Entry(window, width=30)
+    street_entry.pack()
+
+    apartment_label = tk.Label(window, text="Apartment Name:", font=("Arial", 12))
+    apartment_label.pack(pady=10)
+
+    apartment_entry = tk.Entry(window, width=30)
+    apartment_entry.pack()
+
+    submit_button = tk.Button(window, text="Submit", command=submit_rprt, width=15)
+    submit_button.pack(pady=20)
+
+    button1 = tk.Button(window, text="Return Upper Menu", command=user_menu, width=30, height=2)
+    button1.pack(pady=5)
+
+    result_label = tk.Label(window, text="", font=("Arial", 12))
+    result_label.pack()
+
+    window.mainloop()
+
+def supplies():
+    supplies_list = []
+
+    def add_supply():
+        supply = supply_entry.get()
+        if supply:
+            supplies_list.append(supply)
+            supply_entry.delete(0, tk.END)
+
+    def submit_supply():
+        province = province_entry.get()
+        district = district_entry.get()
+
+        for supply in supplies_list:
+            supply_report = [province, district, supply]
+            with open("SupplyReport.csv", "a", newline='', encoding='utf-8') as s:
+                writer = csv.writer(s)
+                writer.writerow(supply_report)
+
+        result_label.config(text="REPORT SUBMITTED")
+
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Supply Request")
+
+    province_label = tk.Label(window, text="Province:", font=("Arial", 12))
+    province_label.pack(pady=10)
+
+    province_entry = tk.Entry(window, width=30)
+    province_entry.pack()
+
+    district_label = tk.Label(window, text="District:", font=("Arial", 12))
+    district_label.pack(pady=10)
+
+    district_entry = tk.Entry(window, width=30)
+    district_entry.pack()
+
+    supply_label = tk.Label(window, text="Supplies:", font=("Arial", 12))
+    supply_label.pack(pady=10)
+
+    supply_entry = tk.Entry(window, width=30)
+    supply_entry.pack()
+
+    add_button = tk.Button(window, text="Add Supply", command=add_supply, width=15)
+    add_button.pack(pady=5)
+
+    submit_button = tk.Button(window, text="Submit", command=submit_supply, width=15)
+    submit_button.pack(pady=20)
+
+    button1 = tk.Button(window, text="Return Upper Menu", command=user_menu, width=30, height=2)
+    button1.pack(pady=5)
+
+    result_label = tk.Label(window, text="", font=("Arial", 12))
+    result_label.pack()
+
+    window.mainloop()
+
+def admin_menu():
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Admin Menu")
+
+    title_label = tk.Label(window, text="Select an option:", font=("Arial", 16, "bold"))
+    title_label.pack(pady=10)
+
+    options_frame = tk.Frame(window)
+    options_frame.pack()
+
+    button1 = tk.Button(options_frame, text="See Information About Earthquake Reports", command=show_report_table, width=35, height=2)
+    button1.pack(pady=5)
+
+    button2 = tk.Button(options_frame, text="See Information About Supply Requests", command=show_supply_table, width=35, height=2)
+    button2.pack(pady=5)
+
+    button3 = tk.Button(options_frame, text="View General Data Reports", command=report, width=35, height=2)
+    button3.pack(pady=5)
+
+    button4 = tk.Button(options_frame, text="Return To Main Menu", command=main_menu, width=30, height=2)
+    button4.pack(pady=5)
+
+    window.mainloop()
+    
+def report():
+    def show_most_requested_supplies():
+        supply_dict = {}
+        for supply in pd.read_csv("SupplyReport.csv")["NEEDED SUPPLY"]:
+            supply_dict[supply] = supply_dict.get(supply, 0) + 1
+
+        fig.clear()
+        plt.pie(supply_dict.values(), labels=supply_dict.keys(), autopct="%1.1f%%")
+        plt.title("Most Requested Supplies")
+        canvas.draw()
+
+    def show_report_distribution_by_city():
+        province_dict = {}
+        for province in pd.read_csv("DebrisReport.csv")["PROVINCE"]:
+            province_dict[province] = province_dict.get(province, 0) + 1 
+
+        fig.clear()
+        plt.pie(province_dict.values(), labels=province_dict.keys(), autopct="%1.1f%%")
+        plt.title("Report Distribution By City")
+        canvas.draw()
+
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Report Menu")
+
+    label = tk.Label(window, text="Select a report option:")
+    label.pack(pady=10)
+
+    button_frame = tk.Frame(window)
+    button_frame.pack()
+
+    button1 = tk.Button(button_frame, text="View Most Requested Supplies", command=show_most_requested_supplies, width=30, height=2, bg="blue", fg="white")
+    button1.pack(side=tk.LEFT, padx=5)
+
+    button2 = tk.Button(button_frame, text="View Report Distribution By City", command=show_report_distribution_by_city, width=30, height=2, bg="green", fg="white")
+    button2.pack(side=tk.LEFT, padx=5)
+
+    fig = plt.figure(figsize=(6, 6))
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.get_tk_widget().pack()
+
+    return_button = tk.Button(window, text="Return to Upper Menu", command=admin_menu, width=30, height=2, bg="red", fg="white")
+    return_button.pack(pady=10)
+
+    window.mainloop()
+
+earthquake_data = [] 
+def recent_earthquakes():
+
+    def fetch_earthquake_data():
+        global earthquake_data
+
+        url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+        infos = {
+            "format": "geojson",
+            "minmagnitude": "2.5",
+            "limit": "5",
+            "minlatitude": "36.0",
+            "maxlatitude": "42.0",
+            "minlongitude": "26.0",
+            "maxlongitude": "45.0"
+        }
+
+        response = requests.get(url, params=infos)
+        data = response.json()
+
+        for veri in data["features"]:
+            place = "PLACE: " + str(veri["properties"]["place"])
+            magnitude = "MAGNITUDE: " + str(veri["properties"]["mag"])
+            time_str = "TIME: " + str(datetime.fromtimestamp(veri["properties"]["time"] / 1000.0))
+
+            earthquake_data.append(place + "\n" + magnitude + "\n" + time_str + "\n\n")
+
+        
+        data_text.delete('1.0', tk.END)
+
+        
+        for data in earthquake_data:
+            data_text.insert(tk.END, data)
+
+    
+        data_text.tag_configure("center", justify="center")
+        data_text.tag_add("center", "1.0", "end")
+
+
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Real-Time Earthquake Data")
+
+    window_width = window.winfo_screenwidth()
+    window_height = window.winfo_screenheight()
+    window.geometry("{}x{}".format(window_width, window_height))
+
+    data_text = tk.Text(window, font=("Courier New", 12))
+    data_text.pack(fill=tk.BOTH, expand=True)
+
+    button = tk.Button(window, text="Return To Main Menu", command=main_menu, width=30, height=2)
+    button.pack(pady=5) 
+
+    fetch_button = tk.Button(window, text="Get Data", command=fetch_earthquake_data, bg="#4caf50", fg="white", width=15, height=2)
+    fetch_button.pack(pady=10)
+
+    for data in earthquake_data:
+        data_text.insert(tk.END, data)
+
+    data_text.tag_configure("center", justify="center")
+    data_text.tag_add("center", "1.0", "end")
+
+    window.mainloop()
+
+def show_report_table():
+    dict_of_file = pd.read_csv("DebrisReport.csv").to_dict("list")
+    list_of_status = dict_of_file["REPORT STATUS"]
+
+    for i in range(len(list_of_status)):
+        if list_of_status[i] != "Help Arrived":
+            list_of_status[i] = random.choice(["Help Arrived", "No Help Yet","No Help Yet","No Help Yet"]) #increasing the probability of "No Help Yet" to make the program more realistic
+
+    dict_of_file["REPORT STATUS"] = list_of_status
+    pd.DataFrame.from_dict(dict_of_file).to_csv("DebrisReport.csv", index=False)
+    
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Earthquake Report Data")
+
+    treeview = ttk.Treeview(window)
+    treeview.pack(fill="both", expand=True)
+
+    data = pd.read_csv("DebrisReport.csv")
+
+    treeview["columns"] = list(data.columns)
+    treeview.heading("#0", text="Index", anchor="w")
+
+    button = tk.Button(window, text="Return To Upper Menu", command=admin_menu, width=30, height=2)
+    button.pack(pady=5) 
+
+    for column in data.columns:
+        treeview.heading(column, text=column, anchor="w")
+
+    index = 1
+    for _, row in data.iterrows():
+        values = row.values.tolist()
+        treeview.insert("", "end", text=index, values=values)
+        index += 1
+
+    window.mainloop()
+
+def show_supply_table():
+    window = tk.Tk()
+    window.attributes("-fullscreen", True)
+    window.title("Supply Report Data")
+
+    treeview = ttk.Treeview(window)
+    treeview.pack(fill="both", expand=True)
+
+    data = pd.read_csv("SupplyReport.csv")
+
+    treeview["columns"] = list(data.columns)
+    treeview.heading("#0", text="Index", anchor="w") 
+
+    button = tk.Button(window, text="Return To Upper Menu", command=admin_menu, width=30, height=2)
+    button.pack(pady=5) 
+
+    for column in data.columns:
+        treeview.heading(column, text=column, anchor="w")
+
+    index = 1
+    for _, row in data.iterrows():
+        values = row.values.tolist()
+        treeview.insert("", "end", text=index, values=values)
+        index += 1
+
+    window.mainloop()
+
+
+
+#initialize the files
+with open('DebrisReport.csv','w', newline='',encoding='utf-8') as file2:
+    debris_data = [
+    ["PROVINCE", "DISTRICT", "STREET", "APARTMENT NAME", "HEALTH CONDITION", "REPORT STATUS"],
+    ["Kahramanmaras", "Elbistan", "(strtname)", "Gul", "Heavily Injured", "No Help Yet"],
+    ["Hatay", "Iskenderun", "(strtname)", "Cennet", "Injured", "No Help Yet"],
+    ["Gaziantep", "Karkamis", "(strtname)", "Nesil", "Heavily Injured", "No Help Yet"],
+    ["Malatya", "Darende", "(strtname)", "Seref", "Injured", "No Help Yet"],
+    ["Diyarbakir", "Sur", "(strtname)", "Zafer", "Injured", "No Help Yet"],
+    ["Sanliurfa", "Harran", "(strtname)", "Dogus", "Injured", "No Help Yet"],
+    ["Adiyaman", "Golbasi", "(strtname)", "Gunes", "No Injuries", "No Help Yet"],
+    ["Osmaniye", "Kadirli", "(strtname)", "Sahin", "Heavily Injured", "No Help Yet"],
+    ["Adana", "Ceyhan", "(strtname)", "Cinar", "Heavily Injured", "No Help Yet"],
+    ["Malatya", "Arguvan", "(strtname)", "Hayat", "Injured", "No Help Yet"],
+    ["Diyarbakir", "Bismil", "(strtname)", "Emek", "No Injuries", "No Help Yet"],
+    ["Kilis", "Mazidagi", "(strtname)", "Huzur", "Injured", "No Help Yet"],
+    ["Sanliurfa", "Harran", "(strtname)", "Gonul", "Heavily Injured", "No Help Yet"],
+    ["Adiyaman", "Samsat", "(strtname)", "Gozde", "No Injuries", "No Help Yet"],
+    ["Osmaniye", "Kadirli", "(strtname)", "Isik", "Heavily Injured", "No Help Yet"],
+    ["Adana", "Kozan", "(strtname)", "Deniz", "Heavily Injured", "No Help Yet"],
+    ["Kahramanmaras", "Pazarcik", "(strtname)", "Nazar", "Injured", "No Help Yet"],
+    ["Hatay", "Dörtyol", "(strtname)", "Gundogan", "No Injuries", "No Help Yet"]]
+    writer = csv.writer(file2)
+    writer.writerows(debris_data)
+
+with open('SupplyReport.csv','w', newline='',encoding='utf-8') as file1:
+    supply_data = [
+    ['PROVINCE', 'DISTRICT', 'NEEDED SUPPLY'],
+    ['Hatay', 'Dörtyol', 'Food'],
+    ['Kahramanmaras', 'Göksun', 'Clothes'],
+    ['Osmaniye', 'Kadirli', 'Water'],
+    ['Adiyaman', 'Merkez', 'Tent'],
+    ['Adana', 'Merkez', 'Medicine'],
+    ['Gaziantep', 'Araban', 'Food'],
+    ['Sanliurfa', 'Bozova', 'Food'],
+    ['Diyarbakir', 'Bismil', 'Medicine'],
+    ['Malatya', 'Yeşilyurt', 'Water'],
+    ['Hatay', 'Iskenderun', 'Tent'],
+    ['Kahramanmaras', 'Pazarcık', 'Tent'],
+    ['Osmaniye', 'Düziçi', 'Water'],
+    ['Adiyaman', 'Samsat', 'Food'],
+    ['Adana', 'Ceyhan', 'Clothes'],
+    ['Kilis', 'Merkez', 'Clothes']]
+    writer = csv.writer(file1)
+    writer.writerows(supply_data)
+
+main_menu()
+        
+            
